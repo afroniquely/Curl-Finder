48d89ccfb404feac39366483828966a161f4db49
Sasha-Shae Weekes <lovesweetshae13@gmail.com>
2016-06-12 17:05:40 -0400

    visibility on load fixed

--- game.js
+++ game.js
@@ -1,6 +1,6 @@
 console.log('Let the safari adventure begin');
 
-var kittenSings = ['.dia1', 'dia2', 'dia3', 'dia4', 'dia5'];
+var kittenSings = ['.dia1', '.dia2', '.dia3', '.dia4', '.dia5'];
 
 $(document).ready(function() {
 
@@ -10,23 +10,27 @@ $(document).ready(function() {
      // $('button').click(function() {
      //     var randomSequence = Math.floor(Math.random()*kittenSings.length);
 
-  //Selecting all the diamond shapes
-  var diamondShapes = document.querySelectorAll('.diamonds img').style.visibility ='hidden';
+  //Selecting all the diamond shapes and setting the images to hidden on screen load.
+  var diamondShapes = document.querySelectorAll('.diamonds img');
+    for (var k = 0; k < diamondShapes.length; k++){
+      diamondShapes[k].style.visibility = 'hidden';
+    }
     console.log(diamondShapes);
 
- var letsPlay = function(){
-    for (var k = 0; k < diamondShapes.length; k++){
-      diamondShapes[k].addEventListener('click', clickHandling);
+ // Starting the game by clicking the Let's Play button
+  var letsPlay = function(){
+    for (var i = 0; i < diamondShapes.length; i++){
+      diamondShapes[i].addEventListener('click', clickHandling);
+      }
     }
-  }
 
   function showImages() {var img = document.getElementsByClassName('hide');}
 
-//The Click Handling event
-  var clickHandling = function(){
-    console.log('you clicked!');
+  //The Click Handling event
+    var clickHandling = function(){
+      console.log('you clicked!');
 
-  };
+    };
 
-  letsPlay();
+    letsPlay();
 })
